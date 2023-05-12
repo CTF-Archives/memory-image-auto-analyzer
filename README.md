26bdd8f2e3f18819c7722a77c0d278009b4f3541
Randark_JMT <randark@foxmail.com>
2023-05-12 11:14:14 +0800

    更新函数结构

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # Memory image auto-analyzer
 ## 一个雏形，一个基于Volatility进行可视化、自动化内存镜像分析的工具
+
+## TODO - 项目规划-windows
+- 建立一键分析，包括imageinfo，pslist，pstree，filescan（加入常见目录筛选），ishistory和cmdscan
+- pslist和pstree整合进树状组件，支持选中进程进行dump（memdump和procdump）
+- 加入网络分析，包含netscan
+- 加入凭据分析，包含lsadump，hashdump和mimikatz（需要处理crypto库）
+- filescan加入常见结果筛选，加入正则搜索，加入提取功能
+- 加入常见信息提取，如剪贴板，桌面截图
+
+## 正则示例
+```shell
+vol.py -f GULF-PC-20220321-064240.raw --profile=Win7SP1x64 filescan | grep -iE "flag|.zip$|.rar$|.7z$|.txt$|.png$|.jpg$|.gif$|.pdf$|.doc$|.docx$|.pcap$|.pcapng$|.raw$|.kdbx$|Desktop\\\{1}.+"
+```
--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 import sys
 import os
-from PySide6.QtCore import QProcess, QSize, QSettings
+from PySide6.QtCore import QProcess, QSettings, Qt
 from PySide6.QtWidgets import *
-from PySide6.QtGui import QAction, QIcon
+from PySide6.QtGui import QAction
 import qdarkstyle
 import logging
-import icon.icon_rc
+from qtawesome import icon
 from layout.QTextEditLogger import QTextEditLogger
 from backend.vol import Worker
 
@@ -20,9 +20,10 @@ class MainWindow(QMainWindow):
         super(MainWindow, self).__init__()
 
         self.setWindowTitle("Memory image auto-analyzer")
+        self.setMinimumSize(800, 500)
 
         # 设置项储存
-        self.settings = QSettings()
+        # self.settings = QSettings()
 
         # 设置菜单栏
         self.set_MenuBar()
@@ -30,44 +31,55 @@ class MainWindow(QMainWindow):
         # 设置状态栏
         self.setStatusBar(QStatusBar(self))
 
-        layout = QGridLayout()
-        self.setLayout(layout)
+        # 设置日志窗体
+        self.set_LogTextBox()
 
+        # 设置工具栏
+        self.set_ToolBar()
+
+        self.ToolsBTN = QPushButton('go P1', self)
+        self.stack = QStackedWidget(self)
+        self.stack.addWidget(self.ToolsBTN)
+        self.setCentralWidget(self.stack)
+        self.show()
+
+    def set_MenuBar(self):
+        menu_bar = self.menuBar()
+        menu_bar.setContextMenuPolicy(Qt.PreventContextMenu)
+        menu_bar_size = menu_bar.font()
+        menu_bar_size.setPointSize(9)
+        menu_bar.setFont(menu_bar_size)
+
+        # 设置文件菜单栏
+        menu_file = menu_bar.addMenu("文件")
+        menu_file.setFont(menu_bar_size)
+        action_OpenNewFile = QAction(icon("fa5.file"), "打开内存镜像文件", self)
+        action_OpenNewFile.setStatusTip("打开内存镜像文件")
+        action_OpenNewFile.triggered.connect(self.OpenFile)
+        menu_file.addAction(action_OpenNewFile)
+        menu_file.addSeparator()
+        action_ApplicationQuit = QAction(icon("fa5s.door-open"), "退出", self)
+        action_ApplicationQuit.setStatusTip("退出程序")
+        action_ApplicationQuit.triggered.connect(self.ApplicationQuit)
+        menu_file.addAction(action_ApplicationQuit)
+
+        # 设置帮助菜单栏
+        menu_help = menu_bar.addMenu("帮助")
+        menu_help.setFont(menu_bar_size)
+        action_ShowLog = QAction(icon("ri.newspaper-line"), "显示日志窗口", self)
+        action_ShowLog.setStatusTip("显示程序日志")
+        action_ShowLog.triggered.connect(self.show_log)
+        menu_help.addAction(action_ShowLog)
+
+    def set_ToolBar(self):
         self.toolbar = QToolBar("My main toolbar")
+        self.toolbar.setContextMenuPolicy(Qt.PreventContextMenu)
         self.toolbar.setMovable(False)
-        self.toolbar.setIconSize(QSize(16, 16))
         self.addToolBar(self.toolbar)
-
-        self.groupbox = QGroupBox("GroupBox Example")
-        layout.addWidget(self.groupbox)
-
-        self.logTextBox = QTextEditLogger(self)
-        self.logTextBox.setFormatter(logging.Formatter('%(asctime)s %(levelname)s %(module)s %(funcName)s %(message)s'))
-        logging.getLogger().addHandler(self.logTextBox)
-        logging.getLogger().setLevel(logging.DEBUG)
-
-        vbox = QVBoxLayout()
-        self.groupbox.setLayout(vbox)
-        vbox.addWidget(self.logTextBox.widget)
-
-        button_action = QAction("add log", self)
-        button_action.setStatusTip("This is your button")
-        button_action.triggered.connect(self.add_log)
-        self.toolbar.addAction(button_action)
 
         button_action = QAction("show log", self)
         button_action.setStatusTip("This is your button")
         button_action.triggered.connect(self.show_log)
-        self.toolbar.addAction(button_action)
-
-        button_action = QAction("hide log", self)
-        button_action.setStatusTip("This is your button")
-        button_action.triggered.connect(self.hide_log)
-        self.toolbar.addAction(button_action)
-
-        button_action = QAction("select file", self)
-        button_action.setStatusTip("This is your button")
-        button_action.triggered.connect(self.OpenFIle)
         self.toolbar.addAction(button_action)
 
         button_action = QAction("imageinfo", self)
@@ -75,39 +87,33 @@ class MainWindow(QMainWindow):
         button_action.triggered.connect(self.start_process)
         self.toolbar.addAction(button_action)
 
-        self.p = None
+    def set_LogTextBox(self):
+        layout = QGridLayout()
+        self.setLayout(layout)
+        self.groupbox = QGroupBox("GroupBox Example")
+        layout.addWidget(self.groupbox)
 
-    def set_MenuBar(self):
-        menu_bar = self.menuBar()
+        self.logTextBox = QTextEditLogger(self)
+        self.logTextBox.setFormatter(logging.Formatter('%(asctime)s - %(levelname)s\n--> module: %(module)s\n--> funcName: %(funcName)s\n--> %(message)s'))
+        logging.getLogger().addHandler(self.logTextBox)
+        logging.getLogger().setLevel(logging.DEBUG)
 
-        # 设置文件菜单栏
-        file_menu = menu_bar.addMenu("文件")
-        action_OpenNewFile = QAction(QIcon(":file-new.svg"), "打开内存镜像文件", self)
-        action_OpenNewFile.setStatusTip("打开内存镜像文件")
-        action_OpenNewFile.triggered.connect(self.OpenFIle)
-        file_menu.addAction(action_OpenNewFile)
-
-        # 增加分隔
-        file_menu.addSeparator()
-
-    def hide_log(self):
-        self.groupbox.setVisible(False)
+        vbox = QVBoxLayout()
+        self.groupbox.setLayout(vbox)
+        vbox.addWidget(self.logTextBox.widget)
 
     def show_log(self):
         self.groupbox.setVisible(True)
 
-    def add_log(self, s):
-        logging.debug('damn, a bug')
-        logging.info('something to remember')
-        logging.warning('that\'s not right')
-        logging.error('foobar')
-
-    def OpenFIle(self):
-        filename, filter = QFileDialog.getOpenFileName(parent=self, caption='Open file', dir='.', filter='*')
+    def OpenFile(self):
+        filename = QFileDialog.getOpenFileName(parent=self, caption='Open file', dir='.', filter='*')
         if filename:
             print(filename)
             config["imagefile"] = filename
             logging.info("select image file:" + filename)
+
+    def ApplicationQuit(self):
+        QApplication.instance, quit()
 
     def message(self, s):
         logging.info(s)
@@ -118,7 +124,7 @@ class MainWindow(QMainWindow):
             dlg = QMessageBox(self)
             dlg.setWindowTitle("Warning!")
             dlg.setText("未选择有效的内存镜像文件!")
-            dlg.exec_()
+            dlg.exec()
             return 0
         if self.p is None:  # No process running.
             self.message("Executing process")
@@ -154,7 +160,6 @@ class MainWindow(QMainWindow):
 
 
 if __name__ == "__main__":
-    # faulthandler.enable(open("./test.out", "w+"), all_threads=True)
     app = QApplication(sys.argv)
     app.setStyleSheet(qdarkstyle.load_stylesheet())
     window = MainWindow()
