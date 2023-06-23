264ba2cc57a3fcb09537968d23c05388414e1be5
Randark_JMT <randark@foxmail.com>
2023-06-23 21:43:28 +0800

    整理一下test文件

--- test.py
+++ test.py
@@ -1,53 +1,109 @@
-from PySide6.QtCore import Qt
-from PySide6.QtWidgets import *
-from PySide6.QtGui import QAction
 import sys
+import os
+sys.path.append(os.environ.get('PS_SITEPACKAGES'))
+from PySide6 import QtGui, QtWidgets, QtCore
 
-# 创建 QApplication 实例
-app = QApplication(sys.argv)
+from functools import partial
 
-# 创建 QWidget 实例
-window = QWidget()
 
-# 创建 QTableWidget 实例
-table_widget = QTableWidget()
+class FilterProxy(QtCore.QSortFilterProxyModel):
+    def __init__(self, *args, **kwargs):
+        super(FilterProxy, self).__init__(*args, **kwargs)
+        self.filters_colums = set()
+        self.mText = ""
 
-# 定义表格数据
-data = [
-    ['A1', 'B1', 'C1'],
-    ['A2', 'B2', 'C2'],
-    ['A3', 'B3', 'C3']
-]
+    def setText(self, text):
+        self.mText = text
+        self.invalidateFilter()
 
-# 设置表格的行数和列数
-table_widget.setRowCount(len(data))
-table_widget.setColumnCount(len(data[0]))
+    def appendColumn(self, name):
+        self.filters_colums.add(name.lower())
+        self.invalidateFilter()
 
-# 将数据添加到表格中
-for i, row in enumerate(data):
-    for j, item in enumerate(row):
-        table_widget.setItem(i, j, QTableWidgetItem(item))
+    def removeColumn(self, name):
+        self.filters_colums.discard(name.lower())
+        self.invalidateFilter()
 
-# 设置表头标签
-header_labels = ['Column 1', 'Column 2', 'Column 3']
-table_widget.setHorizontalHeaderLabels(header_labels)
+    def filterAcceptsRow(self, source_row, source_parent):
+        if self.mText:
+            for i in range(self.sourceModel().columnCount()):
+                header = self.sourceModel().headerData(i, QtCore.Qt.Horizontal) 
+                text = self.sourceModel().index(source_row, i).data()
+                if header in self.filters_colums and self.mText in text.lower():
+                    return True
+            return False
+        return True
 
-# 设置第一列的宽度为固定值
-table_widget.setColumnWidth(0, 100)
 
-# 设置其他列的宽度自适应窗口大小
-table_widget.horizontalHeader().setSectionResizeMode(1, QHeaderView.Stretch)
-table_widget.horizontalHeader().setSectionResizeMode(2, QHeaderView.Stretch)
+class QDictTableView(QtWidgets.QWidget):
+    def __init__(self, *args, **kwargs):
+        super(QDictTableView, self).__init__(*args, **kwargs)
+        self.resize(400,300)
 
-# 创建垂直布局管理器，并设置窗口的布局为该管理器
-layout = QVBoxLayout()
-window.setLayout(layout)
+        # controls
+        self.ui_search_input = QtWidgets.QLineEdit()
+        self.ui_search_input.setPlaceholderText('Search...')
 
-# 将表格添加到布局中
-layout.addWidget(table_widget)
+        self.ui_item_table = QtWidgets.QTableView()
+        self.ui_item_table.setEditTriggers(QtWidgets.QAbstractItemView.NoEditTriggers)
+        self.ui_item_table.setSelectionBehavior(QtWidgets.QAbstractItemView.SelectRows)
+        self.ui_item_table.verticalHeader().hide()
 
-# 显示窗口
-window.show()
+        lay_filters = QtWidgets.QHBoxLayout()
+        lay_main = QtWidgets.QVBoxLayout(self)
+        lay_main.setAlignment(QtCore.Qt.AlignTop)
+        lay_main.addWidget(self.ui_search_input)
+        lay_main.addLayout(lay_filters)
+        lay_main.addWidget(self.ui_item_table)
 
-# 运行应用程序
-sys.exit(app.exec())
+        model = self.populate_table()
+        self.proxy = FilterProxy()
+        self.proxy.setSourceModel(model)
+        self.ui_item_table.setModel(self.proxy)
+
+        for text in ("Name", "Age", "Career"):
+            checkbox = QtWidgets.QCheckBox(text)            
+            checkbox.stateChanged.connect(partial(self.update_columns, text))
+            checkbox.setChecked(True)
+            lay_filters.addWidget(checkbox)
+
+        self.ui_search_input.textChanged.connect(self.proxy.setText)
+
+    def update_columns(self, text, state):
+        cols = self.ui_item_table.model().columnCount()
+        i = [self.ui_item_table.model().headerData(i, QtCore.Qt.Horizontal) for i in range(cols)].index(text.lower())
+        self.ui_item_table.setColumnHidden(i, state == QtCore.Qt.Unchecked)
+        if state == QtCore.Qt.Unchecked:
+            self.proxy.removeColumn(text)
+        else:
+            self.proxy.appendColumn(text)
+
+    def populate_table(self):
+        peoples = [
+            {'name': 'Kevin', 'age': 5, 'career': 'athlete'},
+            {'name': 'Maggie', 'age': 13, 'career': 'banker'},
+            {'name': 'Leslie', 'age': 32, 'career': 'banker'},
+            {'name': 'Emily', 'age': 45, 'career': 'athlete'},
+            {'name': 'David', 'age': 27, 'career': 'banker'},
+            {'name': 'Marie', 'age': 63, 'career': 'secretary'}
+        ]
+
+        model = QtGui.QStandardItemModel()
+
+        headers = ["name", "age", "career"]
+        model.setHorizontalHeaderLabels(headers)
+
+        for row, people in enumerate(peoples):
+            items = []
+            for key, value in people.items():
+                col = headers.index(key)
+                item = QtGui.QStandardItem(str(value))
+                items.append(item)
+            model.insertRow(row, items)
+        return model
+
+if __name__ == '__main__':
+    app = QtWidgets.QApplication(sys.argv)
+    ex = QDictTableView()
+    ex.show()
+    sys.exit(app.exec_())
