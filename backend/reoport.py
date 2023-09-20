2d5c39c700872706caa8f6507a1a638a621944a8
Randark <randark@foxmail.com>
2023-09-20 08:15:05 +0800

    创建报告核心

---
+++ backend/reoport.py
@@ -0,0 +1,15 @@
+import pandas
+from backend.core import core_res
+
+FileType = ["Txt", "Excel", "Markdown", "Html"]
+
+
+class report:
+    def __init__(self) -> None:
+        super().__init__()
+
+    def generate_excel(self):
+        pass
+
+
+core_report = report()
